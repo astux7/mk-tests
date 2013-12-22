12d895337ffe063ce52f80db18412134d1aaecb2
astux7 <Asta.bevainyte@gmail.com>
2013-12-22 17:44:20 +0000

    refactoring code

--- lib/image.rb
+++ lib/image.rb
@@ -3,20 +3,39 @@ require_relative 'pixel'
 class Image
 
   attr_reader :m, :n
-  attr_accessor :pixels, :c
+  attr_accessor :pixels, :color
 
-  def initialize(m, n, c)
-    raise "Image must be in range 1 <= M,N <= 250 px" if !check_image_range?(m,n)
-    @pixels = []
-    for x in 1..m
-      for y in 1..n
-        @pixels << Pixel.new(x,y,c)
-      end
-    end
+  def initialize(m, n, color = 'O')
+    @m, @n =   m, n
+    raise "Image must be in range 1 <= M,N <= 250 px" if !check_image_range?
+    @pixels = Array.new(@m,Array.new(@n,''))
+    create_image(color)
   end
 
-  def check_image_range?(m,n)
-    return true if check_range?(m) && check_range?(n)
+
+  
+
+  def pixels_count
+    counter = 0
+    for x in 0..@m-1
+      for y in 0..@n-1
+        counter +=1
+      end
+    end
+    counter
+  end
+
+  def create_image(color)
+    for x in 0..@m-1
+      for y in 0..@n-1
+        @pixels[x][y] = Pixel.new(x,y,color)
+      end
+    end
+    @pixels
+  end
+
+  def check_image_range?
+    return true if check_range?(@m) && check_range?(@n)
     false
   end
 
@@ -24,6 +43,24 @@ class Image
     return false if size > 250 || size < 1
     true
   end
+  
+  def clear(color = 'C')
+    for x in 0..@m-1
+      for y in 0..@n-1
+        @pixels[x][y].color = color
+      end
+    end
+    @pixels
+  end
 
-
+  def inspect
+   format_string = ""
+    for xx in 0..@m-1
+      for yy in 0..@n-1
+        format_string += @pixels[xx][yy].color
+      end
+      format_string += '\n' 
+    end 
+    print format_string.chomp('\n')
+  end
 end
--- spec/image_spec.rb
+++ spec/image_spec.rb
@@ -1,11 +1,24 @@
 require_relative '../lib/image'
 
 describe Image do
-  let(:img){Image.new(2,3,'O')}
+  let(:img){Image.new(4,5)}
   it 'should create an image' do
-     expect(img.pixels.count).to eq(6)
+     expect(img.pixels_count).to eq(20)
   end
   it 'should raise the error if the MxN out of range 1..250' do 
-    expect(lambda { Image.new(251,6,'0')}).to raise_error(RuntimeError)
+    expect(lambda { Image.new(251,6,'O')}).to raise_error(RuntimeError)
+  end
+  it 'should clear the image' do
+    img.clear
+    expect(img.pixels[1][2].color).to eq('C')
+  end
+  it 'should draw the image as created' do
+    expect(img).to receive(:print).with("OOOOO\\nOOOOO\\nOOOOO\\nOOOOO")
+    img.inspect
+  end
+  it 'should draw the image after cleaning' do
+    img.clear
+    expect(img).to receive(:print).with("CCCCC\\nCCCCC\\nCCCCC\\nCCCCC")
+    img.inspect
   end
 end	
